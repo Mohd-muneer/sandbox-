1e7bbfdd356180a364529eb909c7a4fbf5b7756d
Mohd-muneer <mohdmuneeruddin456@gmail.com>
2023-07-05 12:14:05 +0530

    Custom Hook 1-useFetch

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -2,13 +2,16 @@
 // import UseRefExample2 from "./components/UseRefExample2";
 // import UseRefExample3 from "./components/UseRefExample3"
 // import UseMemoExample from "./components/UseMemoExample";
-import UseCallbackExample from "./components/UseCallbackExample";
+// import UseCallbackExample from "./components/UseCallbackExample";
+
+import CustomhookExample1 from "./components/CustomhookExample1";
+
  
 
 function App() {
   return (
     <div className="conatiner mt-5">
-          <UseCallbackExample/>
+          <CustomhookExample1/>
     </div>
   );
 }
